62fb2f9fa332b0485fdd53488fe7e4af45cd1523
manik <maniksarker652@gmail.com>
2026-04-22 11:19:43 +0600

    create task interface

--- src/app/modules/task/task.interface.ts
+++ src/app/modules/task/task.interface.ts
@@ -1,10 +1,7 @@
 import { Types } from 'mongoose';
 import { ENUM_PAYMENT_STATUS } from '../../utilities/enum';
-import {
-    ENUM_DONE_BY,
-    ENUM_SCHEDULE_TYPE,
-    ENUM_TASK_STATUS,
-} from './task.enum';
+import { ENUM_SERVICE_TYPE } from '../provider/provider.enum';
+import { ENUM_TASK_STATUS } from './task.enum';
 
 export interface IStatusWithDate {
     status: (typeof ENUM_TASK_STATUS)[keyof typeof ENUM_TASK_STATUS];
@@ -13,31 +10,28 @@ export interface IStatusWithDate {
 
 export interface ITask {
     title: string;
-    category: Types.ObjectId;
-    service: Types.ObjectId | null;
+    serviceType: (typeof ENUM_SERVICE_TYPE)[keyof typeof ENUM_SERVICE_TYPE];
     budget: number;
+    taskStartDateTime: Date | null;
     acceptedBidAmount: number;
     customerPayingAmount: number;
     providerEarningAmount: number;
     status: (typeof ENUM_TASK_STATUS)[keyof typeof ENUM_TASK_STATUS];
-    isDeleted: boolean;
     paymentStatus: (typeof ENUM_PAYMENT_STATUS)[keyof typeof ENUM_PAYMENT_STATUS];
     provider?: Types.ObjectId | null;
     customer: Types.ObjectId;
     payOn: string;
-    doneBy: (typeof ENUM_DONE_BY)[keyof typeof ENUM_DONE_BY];
     location: {
         type: 'Point';
         coordinates: [number, number];
     };
     address: string;
-    city: string;
-    scheduleType: (typeof ENUM_SCHEDULE_TYPE)[keyof typeof ENUM_SCHEDULE_TYPE];
+    city?: string;
     preferredDeliveryDateTime?: Date | null;
     description: string;
     task_attachments: string[];
     statusWithDate: IStatusWithDate[];
     transactionId?: string;
-    paymentReferenceId?: string;
     deletedImages: string[];
+    isDeleted: boolean;
 }
