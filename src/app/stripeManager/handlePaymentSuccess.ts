55060472c4f332a058762db97519e1ec1d536baf
manik <maniksarker652@gmail.com>
2026-04-29 10:32:48 +0600

    handle accept bid weehook

--- src/app/stripeManager/handlePaymentSuccess.ts
+++ src/app/stripeManager/handlePaymentSuccess.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-console */
-import { ENUM_PAYMENT_PURPOSE } from '../utilities/enum';
+import { ENUM_TASK_STATUS } from '../modules/task/task.enum';
+import TaskModel from '../modules/task/task.model';
+import { ENUM_PAYMENT_PURPOSE, ENUM_PAYMENT_STATUS } from '../utilities/enum';
 
 const handlePaymentSuccess = async (
     metaData: any,
@@ -8,14 +10,31 @@ const handlePaymentSuccess = async (
     amount: number
 ) => {
     if (metaData.paymentPurpose == ENUM_PAYMENT_PURPOSE.BID_ACCEPT) {
-        await handleBidAcceptPayment(metaData.userId, transactionId, amount);
+        await handleBidAcceptPayment(metaData, transactionId, amount);
     }
 };
 
 const handleBidAcceptPayment = async (
-    userId: string,
+    metaData: any,
     transactionId: string,
     amount: number
-) => {};
+) => {
+    const { taskId } = metaData;
+    const task = await TaskModel.findById(taskId);
+    if (!task) {
+        console.error(`Task with ID ${taskId} not found.`);
+        return;
+    }
+    await TaskModel.findByIdAndUpdate(
+        taskId,
+        {
+            status: ENUM_TASK_STATUS.ASSIGNED,
+            paymentStatus: ENUM_PAYMENT_STATUS.PAID_BY_CUSTOMER,
+            transactionId,
+            amount,
+        },
+        { new: true }
+    );
+};
 
 export default handlePaymentSuccess;
